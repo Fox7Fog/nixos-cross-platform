a02ae7bf9abd420a8733bc4fc7763f90184182eb
fox7fog <fox7fog@protonmail.com>
2025-05-29 15:04:44 -0300

    Add Neovim configuration

---
+++ home/shared/editors/neovim/default.nix
@@ -0,0 +1,65 @@
+{ config, pkgs, lib, ... }:
+
+{
+  programs.neovim = {
+    enable = true;
+    package = if pkgs.stdenv.isDarwin then pkgs.neovim else pkgs.unstable.neovim;
+    
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    
+    extraConfig = ''
+      " Basic settings
+      set number
+      set relativenumber
+      set tabstop=2
+      set shiftwidth=2
+      set expandtab
+      set autoindent
+      set smartindent
+      
+      " Search settings
+      set ignorecase
+      set smartcase
+      set hlsearch
+      set incsearch
+      
+      " UI settings
+      set termguicolors
+      set signcolumn=yes
+      set updatetime=300
+    '';
+    
+    plugins = with pkgs.vimPlugins; [
+      # Essential plugins
+      nvim-treesitter.withAllGrammars
+      telescope-nvim
+      nvim-lspconfig
+      
+      # UI
+      lualine-nvim
+      nvim-web-devicons
+      
+      # Git
+      gitsigns-nvim
+      
+      # File explorer
+      nvim-tree-lua
+      
+      # Completion
+      nvim-cmp
+      cmp-nvim-lsp
+      cmp-buffer
+      cmp-path
+      
+      # Snippets
+      luasnip
+      cmp_luasnip
+      
+      # Language support
+      rust-tools-nvim
+      typescript-nvim
+    ];
+  };
+}

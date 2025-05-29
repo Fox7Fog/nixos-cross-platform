be63885a98e6545439224daa45bfe41bbbe2b9be
fox7fog <fox7fog@protonmail.com>
2025-05-29 14:50:16 -0300

    Add Development Packages configurations

---
+++ home/shared/packages/development.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, lib, ... }:
+
+{
+  home.packages = with pkgs; [
+    # Version control
+    git
+    gh
+    
+    # Build tools
+    gnumake
+    cmake
+    
+    # Language servers
+    nil  # Nix LSP
+    rust-analyzer
+    nodePackages.typescript-language-server
+    python3Packages.python-lsp-server
+    
+    # Debugging
+    gdb
+    lldb
+  ];
+}

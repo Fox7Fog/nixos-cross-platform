ea92b7410d59bec0c14707f9f9c588ba473b599f
fox7fog <fox7fog@protonmail.com>
2025-05-29 14:35:45 -0300

    Add minimal default Nix Flake template

---
+++ templates/default.nix
@@ -0,0 +1,11 @@
+{
+  minimal = {
+    path = ./minimal;
+    description = "Minimal Nix flake template";
+  };
+  
+  development = {
+    path = ./development;
+    description = "Development environment template";
+  };
+}
